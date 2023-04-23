a8550d5a70253ad5603739c6e2f67503fa4d026c
Sk Suraj <104573071+sksuraj153@users.noreply.github.com>
2023-04-23 14:07:29 +0530

    Further Updated

---
+++ crown-clothes/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import { initializeApp} from 'firebase/app';
+import {getAuth,signInWithRedirect,signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {getFirestore,doc,getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgDa240gpHW4g66vGLa6cFg2n0wux6sm0",
+    authDomain: "crown-clothing-db-c159f.firebaseapp.com",
+    projectId: "crown-clothing-db-c159f",
+    storageBucket: "crown-clothing-db-c159f.appspot.com",
+    messagingSenderId: "470577891215",
+    appId: "1:470577891215:web:e930ec39538034efaaadfa"
+  };
+  
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+    prompt: "select_account"
+  });
+
+  export const auth = getAuth();
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
